0f97cd9dc8f72d8783c41a2bd1c41d048e4385ae
neformalized <neformalized@gmail.com>
2024-09-05 12:36:24 +0300

    Stable

--- source/report.py
+++ source/report.py
@@ -1,21 +1,25 @@
 from tensorflow.keras.models import load_model
-#from hood import dataset, image, label
-#import matplotlib.pyplot as plt
+from hood import dataset, image, label
+import matplotlib.pyplot as plt
 import cv2, numpy
+
+model = load_model("/content/model_evaluated.keras")
+
+shape = model.input_shape[1:3]
 
 dataset_path_validation = ["/content/image-to-bb/dataset/validation.txt", "/content/image-to-bb/dataset/validation/"]
 dataset_validation = dataset.Dataset(dataset_path_validation)
 
 for item in dataset_validation.data:
     
-    x = image.process(item[0], self.input_image)
+    x = image.process(item[0], shape)
     y = label.process(item[1])
     
     result = model.predict(numpy.expand_dims(x, axis = 0), verbose = 0)
 
     img  = (x * 255).astype(numpy.uint8)
     
-    cv2.rectangle(img, (int(shape[0] * validation.y[i][0]), int(shape[1] * validation.y[i][1])), (int(shape[0] * validation.y[i][2]), int(shape[1] * validation.y[i][3])), (0, 255, 0), 1)
+    cv2.rectangle(img, (int(shape[0] * y[0]), int(shape[1] * y[1])), (int(shape[0] * y[2]), int(shape[1] * y[3])), (0, 255, 0), 1)
     cv2.rectangle(img, (int(shape[0] * result[0][0]), int(shape[1] * result[0][1])), (int(shape[0] * result[0][2]), int(shape[1] * result[0][3])), (255, 255, 0), 1)
     
     plt.figure(figsize=(3, 2))
