9f38a187c90e5746a9fb325c617a3e1d4c588b52
neformalized <neformalized@gmail.com>
2024-09-05 12:14:17 +0300

    Stable

--- source/report.py
+++ source/report.py
@@ -1,13 +1,19 @@
-import tensorflow.keras.models.load_model
-import matplotlib.pyplot
-import hood.dataset
+from tensorflow.keras.models import load_model
+#from hood import dataset, image, label
+#import matplotlib.pyplot as plt
 import cv2, numpy
-'''
-for i in range(len(validation.x)):
-    
-    result = model.predict(numpy.expand_dims(validation.x[i], axis = 0), verbose = 0)
 
-    img  = (validation.x[i] * 255).astype(numpy.uint8)
+dataset_path_validation = ["/content/image-to-bb/dataset/validation.txt", "/content/image-to-bb/dataset/validation/"]
+dataset_validation = dataset.Dataset(dataset_path_validation)
+
+for item in dataset_validation.data:
+    
+    x = image.process(item[0], self.input_image)
+    y = label.process(item[1])
+    
+    result = model.predict(numpy.expand_dims(x, axis = 0), verbose = 0)
+
+    img  = (x * 255).astype(numpy.uint8)
     
     cv2.rectangle(img, (int(shape[0] * validation.y[i][0]), int(shape[1] * validation.y[i][1])), (int(shape[0] * validation.y[i][2]), int(shape[1] * validation.y[i][3])), (0, 255, 0), 1)
     cv2.rectangle(img, (int(shape[0] * result[0][0]), int(shape[1] * result[0][1])), (int(shape[0] * result[0][2]), int(shape[1] * result[0][3])), (255, 255, 0), 1)
@@ -17,4 +23,3 @@ for i in range(len(validation.x)):
     plt.axis("off")
     plt.show()
 #
-'''
