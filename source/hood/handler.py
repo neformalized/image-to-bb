0176b50781dfb63ccca0943cbb76f7dace61ef3d
neformalized <neformalized@gmail.com>
2024-09-05 06:40:19 +0300

    Stable

--- source/hood/handler.py
+++ source/hood/handler.py
@@ -147,10 +147,10 @@ class Handler():
             
             new_lr = self.model.optimizer.learning_rate * 0.76
             
+            self.model.optimizer.learning_rate = new_lr
+            
             print("new learning_rate")
             print(new_lr)
-            
-            self.model.optimizer.learning_rate = new_lr
         #
         
         if(self.loss_train_current < self.loss_train_record): self.loss_train_record = self.loss_train_current
