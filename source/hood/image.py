cb42be799e714d8a23f3b65162d7f3080573daf6
neformalized <neformalized@gmail.com>
2024-09-14 18:50:02 +0300

    Stable

--- source/hood/image.py
+++ source/hood/image.py
@@ -4,7 +4,7 @@ def process(image_path, target_size):
     
     image = cv2.imread(image_path)
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (target_size[0], target_size[1]))
+    image = cv2.resize(image, (target_size[1], target_size[0]))
     image = image/255
     
     return image
